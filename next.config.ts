01aaad5a7982117f83ead0cc1daa24f2690992c2
Zaymoriel Vandrell <zaymorielvandrell@gmail.com>
2026-03-11 15:05:44 +0800

    chore: bootstrap FocusZen app configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+const config: NextConfig = {
+  reactCompiler: true
 };
 
-export default nextConfig;
+export default config;
